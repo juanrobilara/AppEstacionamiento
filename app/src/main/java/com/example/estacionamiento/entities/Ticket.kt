a40cb40466fd8e28e9c84879e4cae2c2b6c73d73
Juan Robi Lara <juanrobilara1@gmail.com>
2022-11-24 13:49:38 -0300

    se actualizo ticket

--- app/src/main/java/com/example/estacionamiento/entities/Ticket.kt
+++ app/src/main/java/com/example/estacionamiento/entities/Ticket.kt
@@ -1,5 +1,14 @@
 package com.example.estacionamiento.entities
 
-class Ticket {
+import java.time.LocalDate
+import java.time.LocalTime
 
-}
+data class Ticket(
+    val codigo: Int,
+    val fechaIngreso: LocalDate,
+    val horaIngreso: LocalTime,
+    val vehiculo: String,
+    val estadia: Int,
+    val montoBruto: Double,
+    val montoFinal: Double
+)
